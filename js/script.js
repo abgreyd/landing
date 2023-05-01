06d02ecba2d12513d857067047eda4fc7a4821ca
abgreyd <118710149+abgreyd@users.noreply.github.com>
2023-05-01 10:48:30 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener("mousemove", parallax);
+
+function parallax(e) {
+    this.querySelectorAll(".img").forEach(img => {
+        const speed = img.getAttribute("data-speed");
+
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
+
+        img.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
+
+
+/* -=- Tabs -=- */
+let tabs = document.querySelector('.tarifs_tabs'),
+    tabsTitle = document.querySelectorAll('.tarifs_tabs_title'),
+    tarifPrice = document.querySelectorAll('.number'),
+    tarifDuring = document.querySelectorAll('.during');
+
+tabs.addEventListener('click', e => {
+    if (!e.target.classList.contains('tarifs_tabs_title')) return
+
+    tabsTitle.forEach(tab => tab.classList.remove('active'));
+    e.target.classList.add('active');
+
+    if (e.target.dataset.time === 'year') {
+        tarifPrice.forEach(price => price.innerHTML *= 12);
+        tarifDuring.forEach(during => during.innerHTML = "/year");
+    }
+
+    if (e.target.dataset.time === 'month') {
+        tarifPrice.forEach(price => price.innerHTML /= 12);
+        tarifDuring.forEach(during => during.innerHTML = "/month");
+    }
+
+});
+let btnUp = Document.querySelector('.SscrollTop');
+
+btnUp.addEventListener('click',function(){
+    scrollToY(0);
+})
+document.addEventListener('scroll',function(){
+    let cl = btnUp.classList;
+    let threshold = Window.innerHeight / 2;
+    Window.scrollY >threshold ? cl.add('btnUp-open');
+})
